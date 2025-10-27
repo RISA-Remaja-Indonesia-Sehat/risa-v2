2c7efd0ca1b8c3b8e8ecf1767db7da7d76e0edec
Helena <helena200805@gmail.com>
2025-10-27 08:40:22 +0700

    fix error

--- src/app/mini-game/result/page.js
+++ src/app/mini-game/result/page.js
@@ -302,15 +302,3 @@ export default function ResultPage() {
         </div>
     );
 }
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>
-        <h1>Mini Game Result Page</h1>
-        <p>This is where the results of the mini-game will be displayed.</p>
-    </div>
-  )
-}
-
-export default page
