1c203c7cf556f103c68ad85506c7075bf133849d
Helena <helena200805@gmail.com>
2025-10-28 05:38:41 +0700

    coba

--- src/app/landing-page/HeroSection.js
+++ src/app/landing-page/HeroSection.js
@@ -42,17 +42,17 @@ export default function HeroSection() {
   return (
     <section ref={heroRef} className="container my-4 mx-auto" id="hero">
         <div className="p-4">
-        <h1 className="text-3xl md:text-4xl text-center font-bold mb-4 md:mb-6 hero-title opacity-0">Berani Tahu, Berani Lindungi Diri</h1>
-        <p className="text-center text-gray-600 leading-6 mb-8 max-w-2xl mx-auto hero-text opacity-0">Belajar tentang pubertas, cinta, dan kesehatan reproduksi dengan cara yang aman dan mudah dipahami</p>
+        <h1 className="text-3xl md:text-4xl text-center font-bold mb-4 md:mb-6 hero-title opacity-0">Belajar Sambil Bermain Bersama RISA</h1>
+        <p className="text-center text-gray-600 leading-6 mb-8 max-w-2xl mx-auto hero-text opacity-0">Belajar seputar pubertas, cinta, dan kesehatan reproduksi lewat mini games interaktif yang seru dan mudah dipahami</p>
         
         <div className="w-fit mx-auto mb-12 hero-button opacity-0">
             <Link href="/article">
-                <CustomButton className="px-6 py-3 text-base font-semibold" title="Mulai Belajar Sekarang" />
+                <CustomButton className="px-6 py-3 text-base font-semibold" title="Mainkan Game Sekarang" />
             </Link>
         </div>
 
         <div className="w-fit mx-auto">
-            <Image src="/image/hero-image.png" width={600} height={400} alt="Hero" className="hero-image opacity-0 max-w-full h-auto" priority={true} />
+            <Image src="/image/game-1.png" width={600} height={400} alt="Hero" className="hero-image opacity-0 max-w-full h-auto" priority={true} />
         </div>
         </div>
     </section>
